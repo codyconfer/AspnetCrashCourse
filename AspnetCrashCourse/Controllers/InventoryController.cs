803c0ff6458d3c470fcb37fabc1d225c30f5948d
codyconfer <mail@codyconfer.me>
2020-10-08 14:13:55 -0600

    stub out get inventory endpoint in inventory controller

---
+++ AspnetCrashCourse/Controllers/InventoryController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspnetCrashCourse.Controllers
+{
+    [Route("api/inventory")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() =>
+            throw new NotImplementedException();
+    }
+}
