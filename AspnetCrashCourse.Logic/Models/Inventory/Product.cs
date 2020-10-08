dd5f0136041d948c1736b8a0cd6a771e3138a934
codyconfer <mail@codyconfer.me>
2020-10-08 14:09:31 -0600

    Add inventory implementations at logic level

---
+++ AspnetCrashCourse.Logic/Models/Inventory/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AspnetCrashCourse.Schema.Inventory;
+
+namespace AspnetCrashCourse.Logic.Models.Inventory
+{
+    internal class Product : IProduct
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal UnitCost { get; set; }
+        public IProductQuantity Quantity { get; set; }
+    }
+}
