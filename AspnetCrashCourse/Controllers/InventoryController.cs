3eed3f5bcaebce85a9c324fdc9edc23bf9f950db
codyconfer <mail@codyconfer.me>
2020-10-08 14:23:26 -0600

    set up dependency injection for mock inventory service, inject into inventory controller

--- AspnetCrashCourse/Controllers/InventoryController.cs
+++ AspnetCrashCourse/Controllers/InventoryController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspnetCrashCourse.Logic.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,6 +12,13 @@ namespace AspnetCrashCourse.Controllers
     [ApiController]
     public class InventoryController : ControllerBase
     {
+        private readonly IInventoryService InventoryService;
+
+        public InventoryController(IInventoryService inventoryService)
+        {
+            InventoryService = inventoryService;
+        }
+
         [HttpGet]
         public IActionResult Get() =>
             throw new NotImplementedException();
